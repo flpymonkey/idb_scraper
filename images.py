5a03ac7ec06966f7488a14f491bf48bf95dd842c
Benjamin Johnson <bencj96@gmail.com>
2018-02-26 21:56:47 -0600

    Runs scraper to get photos for every national park

--- images.py
+++ images.py
@@ -5,6 +5,9 @@ import pprint
 import requests
 from pickle import dump, load
 
+# store all park photo dicts in here to be pickled
+all_park_photos = []
+
 class REST:
     """
     This class contains some useful functions for using the requests library.
@@ -37,12 +40,12 @@ class ImageScraper:
             key = str(keyfile.readline()).strip()
             self.api_key = key
 
-    def get_photos(self) -> dict:
+    def get_photos(self, text) -> dict:
         path = ""
         params = {"api_key": self.api_key,
                   "method": "flickr.photos.search",
                   "format": "json",
-                  "text":   "yellowstone national park"}
+                  "text":  text}
         response = REST.get_json(self.BASE_URL_, path, params)
         return response
 
@@ -73,10 +76,10 @@ class ImageScraper:
         response = REST.get_json(self.BASE_URL_, path, params)
         return response
 
-if __name__ == "__main__":
+def get_park_photos(name):
     pp = pprint.PrettyPrinter(indent=2)
     image_scraper = ImageScraper()
-    photo_data = image_scraper.get_photos()
+    photo_data = image_scraper.get_photos(name)
     for photo in photo_data["photos"]["photo"]:
         try:
             url = "https://c1.staticflickr.com/{}/{}/{}_{}_h.jpg".format(
@@ -85,22 +88,46 @@ if __name__ == "__main__":
                     photo["id"],
                     photo["secret"])
             print("=" * 60)
-            print("direct url: {}".format(url))
+            #print("direct url: {}".format(url))
             info = image_scraper.get_info(photo["id"])
-            print("date taken: {}".format(info["photo"]["dates"]["taken"]))
-            print("url:        {}".format(info["photo"]["urls"]["url"][0]["_content"]))
-            print("author:     {}".format(info["photo"]["owner"]["realname"]))
-            print("title:      {}".format(info["photo"]["title"]["_content"]))
-            print("description:      {}".format(info["photo"]["description"]["_content"]))
+            #print("date taken: {}".format(info["photo"]["dates"]["taken"]))
+            #print("url:        {}".format(info["photo"]["urls"]["url"][0]["_content"]))
+            #print("author:     {}".format(info["photo"]["owner"]["realname"]))
+            #print("title:      {}".format(info["photo"]["title"]["_content"]))
+            #print("description:      {}".format(info["photo"]["description"]["_content"]))
             #pp.pprint(info)
             exif = image_scraper.get_exif(photo["id"])
             #print("EXIF")
             #pprint(exif)
-            print("camera:     {}".format(exif["photo"]["camera"]))
+            #print("camera:     {}".format(exif["photo"]["camera"]))
             #print("Model: {} \nMake: {}".format(1, 1))
             favorites = image_scraper.get_favorites(photo["id"])
-            print("Favorites:  {}".format(favorites["photo"]["total"]))
+            #print("Favorites:  {}".format(favorites["photo"]["total"]))
+
+            pickle_dict = {}
+            pickle_dict["id"] = photo["id"]
+            pickle_dict["url"] = info["photo"]["urls"]["url"][0]["_content"]
+            pickle_dict["direct url"] = url
+            pickle_dict["date taken"] = info["photo"]["dates"]["taken"]
+            pickle_dict["author"] = info["photo"]["owner"]["realname"]
+            pickle_dict["title"] = info["photo"]["title"]["_content"]
+            pickle_dict["description"] = info["photo"]["description"]["_content"]
+            pickle_dict["camera:"] = exif["photo"]["camera"]
+            pickle_dict["Favorites"] = favorites["photo"]["total"]
+
+            print ("pickling: " + str(pickle_dict))
+            all_park_photos.append(pickle_dict)
         except:
             pass
 
-    print("Done.")
+if __name__ == "__main__":
+    with open("./db.pckl", "rb") as infile:
+        national_parks = load(infile)
+        print(national_parks.keys())
+    for park in national_parks:
+        park_name = park
+        get_park_photos(park_name)
+        print("Done.")
+    with open("./dbpics.pckl", "wb") as outfile:
+        dump(all_park_photos, outfile)
+    print ("Dumped!")
