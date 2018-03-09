c0b654665d458822f8ba23c94c86b71772f3cd75
Jeff Bell <jeff@jhbell.com>
2018-03-09 08:48:07 -0600

    Add a try/except around each execution

--- data_pumper.py
+++ data_pumper.py
@@ -21,18 +21,20 @@ for park in national_parks.keys():
     # print(national_parks[park])
     national_park = national_parks[park]
     print (national_park.keys())
-    q = "INSERT INTO parks VALUES('{}','{}','{}','{}','{}','{}','{}','{}')".format(national_park['fullName'],
-                        national_park['states'],
-                        national_park['latLong'],
-                        national_park['description'],
-                        national_park['directionsInfo'],
-                        national_park['url'],
-                        national_park['weatherInfo'],
-                        national_park['directionsUrl'])
-    result = connection.execute(q)
-#
-# print("Done.")
-#
-#
+    try:
+        q = "INSERT INTO parks VALUES('{}','{}','{}','{}','{}','{}','{}','{}')".format(national_park['fullName'],
+                            national_park['states'],
+                            national_park['latLong'],
+                            national_park['description'],
+                            national_park['directionsInfo'],
+                            national_park['url'],
+                            national_park['weatherInfo'],
+                            national_park['directionsUrl'])
+        result = connection.execute(q)
+    except Exception e:
+        print(e)
+
+print("Done.")
+
 # for row in result:
 #     print(row['name'])
